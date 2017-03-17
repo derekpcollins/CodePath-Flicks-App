0a7f97f1948815834e4aa21a972be1ac957148dd
Derek P. Collins <dcollins@netflix.com>
2017-03-17 13:01:10 -0700

    Updated display text in list view

--- app/src/main/java/com/dcollins/codepathflicksapp/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/dcollins/codepathflicksapp/adapters/MovieArrayAdapter.java
@@ -1,8 +1,15 @@
 package com.dcollins.codepathflicksapp.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.dcollins.codepathflicksapp.R;
 import com.dcollins.codepathflicksapp.models.Movie;
 
 import java.util.List;
@@ -15,5 +22,33 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // get the data item for position
+        Movie movie = getItem(position);
+
+        // check the existing view being reused
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // find the image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // populate data
+        tvTitle.setText(movie.getOrignalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // return the view
+        return convertView;
     }
 }
